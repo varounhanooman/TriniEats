c1732d7cc548216ecd7f929c48988682ab92b79c
Josiah Maharaj <josiahmaharaj@outlook.com>
2019-08-29 23:37:54 -0400

    added firestore

--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Test extends StatelessWidget {
   const Test({Key key}) : super(key: key);
@@ -6,10 +7,36 @@ class Test extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Hello, world!'),
-      ),
+        appBar: AppBar(),
+        body: Container(
+          child: FireData(),
+        ));
+  }
+}
+
+class FireData extends StatelessWidget {
+  const FireData({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection("Promotions").snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return Text('no data');
+        return ListView(
+          children: sumName(snapshot),
+        );
+      },
     );
+  }
+
+  sumName(AsyncSnapshot<QuerySnapshot> snapshot) {
+    return snapshot.data.documents
+        .map((doc) => new ListTile(
+              title: Text(doc["Title"]),
+              subtitle: Text(doc["Company"]),
+              leading: Image.network(doc["Image"]),
+            ))
+        .toList();
   }
 }
