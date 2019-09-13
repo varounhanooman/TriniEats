4b406cb22c170c16f711c3848b1aea6926d232b8
Josiah Maharaj <josiahmaharaj@outlook.com>
2019-09-12 21:12:00 -0400

    cards and buttons added

--- lib/pages/menuitem.dart
+++ lib/pages/menuitem.dart
@@ -21,14 +21,14 @@ class Item extends StatelessWidget {
             flexibleSpace: FlexibleSpaceBar(
               centerTitle: true,
               title: Text(name),
-              background: image,
+              background: Opacity(opacity: 0.7, child: image),
             ),
           )
         ];
       },
       body: Container(
         margin: const EdgeInsets.all(10.0),
-        child: Column(
+        child: ListView(
           children: <Widget>[
             Card(
               child: Padding(
@@ -52,6 +52,72 @@ class Item extends StatelessWidget {
                 ),
               ),
             ),
+            Card(
+              child: Container(
+                margin: EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Ingredients',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    SizedBox(
+                      height: 200,
+                      child: ListView.builder(
+                        itemCount: items.length,
+                        itemBuilder: (context, index) {
+                          return Text(items[index]);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 30, right: 30, top: 20),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.remove),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text('1'),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.add),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 30, right: 30, top: 20),
+              child: RaisedButton(child: Text('Add to Cart'), onPressed: (){},),
+            )
+            // Container(
+            //   margin: EdgeInsets.only(left: 50, right: 50, top: 20),
+            //   child: Row(
+            //     children: <Widget>[
+            //       FloatingActionButton(
+            //         child: Icon(Icons.remove),
+            //         onPressed: () {},
+            //       ),
+            //       Expanded(
+            //           child: Center(
+            //         child: Text('1'),
+            //       )),
+            //       FloatingActionButton(
+            //         child: Icon(Icons.add),
+            //         onPressed: () {},
+            //       )
+            //     ],
+            //   ),
+            // ),
           ],
         ),
       ),
